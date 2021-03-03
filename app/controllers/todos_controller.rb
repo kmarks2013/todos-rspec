bf9a07d6af19e1bd9eb95de682db27af6d30054e
kmarks2013 <kmarks2013@gmail.com>
2021-03-03 14:37:30 -0500

    adds new and create methods

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,12 @@ class TodosController < ApplicationController
     def index
 
     end
+
+    def new
+        @todo = Todo.new
+    end
+
+    def create
+        redirect_to todos_path
+    end
 end
