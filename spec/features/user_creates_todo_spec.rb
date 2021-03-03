003dcd83b1854cb485b35af9df3ae248c9cfaf5f
kmarks2013 <kmarks2013@gmail.com>
2021-03-03 14:05:49 -0500

    first few steps for visiting the root path

--- spec/features/user_creates_todo_spec.rb
+++ spec/features/user_creates_todo_spec.rb
@@ -2,5 +2,11 @@ require 'rails_helper'
 
 feature "User creates a todo" do
     scenario "successfully" do
+        visit root_path
+
+        click_on "Add a new todo"
+
+        fill_in "Title", with: "Buy Milk" #this is for filling out a form
+        click_on "Submit" #this is for clicking on a link or a button
     end
 end
